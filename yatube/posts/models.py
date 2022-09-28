79cfa949866f1a2b4afcad6604bbca699a790731
Nikita Korovyakovskiy@mail.ru <nikita.korovyakovskiy@mail.ru>
2022-09-28 20:18:02 +0700

    Создал модели буджу загружать БД

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,11 +4,31 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Наименование')
+    slug = models.SlugField(unique=True)
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField(verbose_name='Контент')
-    pub_date = models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата публикации'
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Автор'
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name='group',
+        blank=True,
+        null=True,
+        verbose_name='Группа'
     )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Это главная страница проекта Yatube'
     context = {
-        'title': 'Это главная страница проекта Yatube',
+        'posts': posts,
+        'title': title,
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_lists(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+
+    posts = Group.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = 'Здесь будет информация о группах проекта Yatube'
     context = {
-        'title': 'Здесь будет информация о группах проекта Yatube',
+        'group': group,
+        'postes': posts,
+        'title': title,
     }
     return render(request, 'posts/group_list.html', context)
